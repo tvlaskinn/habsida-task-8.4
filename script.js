f9e2aaf85b361f314c4420bf9c854ee6211f2bc6
tvlaskinn <vlaskintimur12@gmail.com>
2026-09-09 15:47:04 +0500

    task-Brands swiper.fixed-8.4

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const brands = document.querySelector('.brands');
 const button = document.querySelector('.brands__button');
+const buttonIcon =document.querySelector('.brands__button-icon');
+const buttonText = document.querySelector('.brands__button-text');
 
 button.addEventListener('click', () => {
     brands.classList.toggle('brands--expanded');
 
     if (brands.classList.contains('brands--expanded')) {
-        button.textContent = 'Скрыть';
+        buttonIcon.src = './img/expand.2.svg';
+        buttonText.textContent = 'Скрыть';
     } else {
-        button.textContent = 'Показать все';
+        buttonText.textContent = 'Показать все';
+        buttonIcon.src = './img/expand.svg';  
     }
 });
 
+
 if (window.innerWidth < 768) {
-    const swiper = new Swiper ('.swiper ', {
+
+    const swiper = new Swiper('.swiper', {
+
         slidesPerView: 1,
         spaceBetween: 16,
+
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
-      
+
     });
+
 }
